3be1e9732161608c81f04bad819e0203eb77b0ef
PetkoBorisov <petkoko99@gmail.com>
2022-04-18 17:11:34 +0300

    Update course.ex

--- lib/course.ex
+++ lib/course.ex
@@ -11,6 +11,10 @@ defmodule Course do
     GenServer.call(__MODULE__, :get_state)
   end
 
+  def logout do
+    GenServer.call(__MODULE__, :logout)
+  end
+
   def get_user(username) do
     GenServer.call(__MODULE__, {:get_user, username})
   end
@@ -45,11 +49,39 @@ defmodule Course do
     {:reply, state, state}
   end
 
+  def handle_call(:logout, _from, state) do
+    isAnyoneLogged = Enum.any?(state, fn x -> x.is_logged_in == true end)
+
+    indexOfUser =
+      if isAnyoneLogged do
+        Enum.find_index(state, fn x -> x.is_logged_in == true end)
+      else
+        nil
+      end
+
+    state =
+      if indexOfUser != nil do
+        List.update_at(state, indexOfUser, fn x -> Map.put(x, :is_logged_in, false) end)
+      else
+        state
+      end
+
+    result =
+      if indexOfUser != nil do
+        "Successfull logout"
+      else
+        "No logged user"
+      end
+
+    {:reply, result, state}
+  end
+
   def handle_call({:get_user, username}, _from, state) do
     {:reply, Enum.filter(state, fn x -> x.username == username end), state}
   end
 
   def handle_call({:login, username, password}, _from, state) do
+    # add complex validation not just success/failed
     isAnyoneLogged = Enum.any?(state, fn x -> x.is_logged_in == true end)
 
     indexOfUser =
@@ -77,7 +109,15 @@ defmodule Course do
   end
 
   def handle_cast({:add, elem}, state) do
-    {:noreply, [elem | state]}
+    # add error if there is a user with the same username or email(now the mothod dont change the state when this happens)
+    new_state =
+      if Enum.any?(state, fn x -> x.username == elem.username || x.email == elem.email end) do
+        state
+      else
+        [elem | state]
+      end
+
+    {:noreply, new_state}
   end
 
   def handle_cast({:delete, username}, state) do
